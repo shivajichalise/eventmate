d32cf2dd2e6dc5481c79b0b2654520c91010ff74
Yakeen Kapali <be2019se36@gces.edu.np>
2023-06-29 17:25:44 +0545

    removing unwanted links from footer

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -2,10 +2,9 @@ export default function Footer() {
     return (
         <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
             <div className="grid grid-flow-col gap-4">
+                <a className="link link-hover">Events</a>
                 <a className="link link-hover">About us</a>
                 <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
             </div>
             <div>
                 <div className="grid grid-flow-col gap-4">
