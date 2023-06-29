e06230a568399cb4d356c9a1c62d64c78b2127ed
Yakeen Kapali <be2019se36@gces.edu.np>
2023-06-29 13:58:43 +0545

    creating payments table

--- database/migrations/2023_06_29_071116_create_payments_table.php
+++ database/migrations/2023_06_29_071116_create_payments_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,11 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('ticket_id')->constrained('tickets')->onDelete('cascade');
+            $table->integer('amount');
+            $table->boolean('paid');
+            $table->string('status');
             $table->timestamps();
         });
     }
