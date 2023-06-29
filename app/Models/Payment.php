fa6f63b20d7a9cefc321148f25a982f38f10a2c6
Yakeen Kapali <be2019se36@gces.edu.np>
2023-06-29 14:07:33 +0545

    adding relationships between User and Payment

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Payment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'ticket_id',
+        'amount',
+        'paid',
+        'status',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
