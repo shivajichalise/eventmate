626e36b2ff728e42d68a77246e8673d09af89c9a
Yakeen Kapali <be2019se36@gces.edu.np>
2023-09-16 12:38:07 +0545

    adding male avatar logo

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -30,7 +30,7 @@
             <div class="card card-primary card-outline">
                 <div class="card-body box-profile">
                     <div class="text-center">
-                        <img class="profile-user-img img-fluid img-circle" src="/images/logo.png" alt="User profile picture">
+                        <img class="profile-user-img img-fluid img-circle" src="/images/male_avatar.png" alt="User profile picture">
                     </div>
                     <h3 class="profile-username text-center">{{ $user->name }}</h3>
                     <!-- <p class="text-muted text-center">Software Engineer</p> -->
