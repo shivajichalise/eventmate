912d3a63bd3b30761a8eb487560549f033fd020b
Shivaji Chalise <respond2shivaji@gmail.com>
2023-10-06 18:59:17 +0545

    ran some tests

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,6 +17,7 @@ class UserController extends Controller
      */
     public function index(UsersDataTable $dataTable)
     {
+        // return User::all();
         return $dataTable->render('users.index');
     }
 
